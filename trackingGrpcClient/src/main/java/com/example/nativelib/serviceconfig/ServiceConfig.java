9e191b732350d58b9ea583a1f4a131fa35703c6f
ankitkumar <ankit.kumar071460@gmail.com>
2024-11-02 13:16:08 +0530

    fix retry

--- trackingGrpcClient/src/main/java/com/example/nativelib/serviceconfig/ServiceConfig.java
+++ trackingGrpcClient/src/main/java/com/example/nativelib/serviceconfig/ServiceConfig.java
@@ -7,6 +7,12 @@ import java.util.Map;
 
 public class ServiceConfig {
     public static Map<String, Object> createServiceConfig() {
+        // Configuration for UserService
+        Map<String, Object> methodConfig = new HashMap<>();
+        methodConfig.put("name", Collections.singletonList(
+                Map.of("service", "org.dlms.services.UserService")
+        ));
+
         Map<String, Object> retryPolicy = new HashMap<>();
         retryPolicy.put("maxAttempts", 10D);
         retryPolicy.put("initialBackoff", "10s");
@@ -14,12 +20,9 @@ public class ServiceConfig {
         retryPolicy.put("backoffMultiplier", 2D);
         retryPolicy.put("retryableStatusCodes", Arrays.asList("UNAVAILABLE", "DEADLINE_EXCEEDED"));
 
-        Map<String, Object> methodConfig = new HashMap<>();
-        methodConfig.put("name", Collections.singletonList(
-                Map.of("service", "org.dlms.services.UserService")
-        ));
         methodConfig.put("retryPolicy", retryPolicy);
         methodConfig.put("waitForReady", true);
+        methodConfig.put("timeout", "5s");
 
         return Map.of("methodConfig", Collections.singletonList(methodConfig));
     }
